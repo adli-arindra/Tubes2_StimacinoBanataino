c6920494f1b1bc6406e7c40e7f036df8201f85fa
drianto <andri070805@gmail.com>
2025-05-11 11:56:24 +0700

    feat: add read JSON file single mode

--- backend/api/main.go
+++ backend/api/main.go
@@ -12,24 +12,23 @@ import (
 )
 
 type Node struct {
-	Name  string `json:"name"`
-	Idx   int    `json:"idx"`
-	Left  *Node  `json:"left,omitempty"`
-	Right *Node  `json:"right,omitempty"`
+	Name           string  `json:"name"`
+	NodeDiscovered int     `json:"node discovered"`
+	Children       []Node  `json:"children"`
 }
 
 type SearchRequest struct {
 	Target     string `json:"target"`
 	Algorithm  string `json:"algorithm"`
 	Mode       string `json:"mode"`
-	MaxRecipes int    `json:"max_recipes,omitempty"`
+	MaxRecipes *int   `json:"max_recipes,omitempty"`
 }
 
 type SearchResponse struct {
-	Trees         Node    `json:"trees"`
-	NumberOfPaths int     `json:"numberOfPaths"`
-	NodesVisited  int     `json:"nodesVisited"`
-	ElapsedTime   float64 `json:"elapsedTime"`
+	Tree         Node    `json:"tree"`
+	Algorithm    string  `json:"algorithm"`
+	Duration     float64 `json:"duration_ms"`
+	VisitedNodes int     `json:"visited_nodes"`
 }
 
 func main() {
@@ -58,35 +57,40 @@ func processSearchRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	jsonFile, err := os.Open("../../frontend/example5.json")
+	var filePath string
+
+	switch req.Algorithm {
+	case "BFS":
+		if req.Mode == "single" {
+			filePath = fmt.Sprintf("../bfs/result_BFS/%s_bfs.json", req.Target)
+		} else {
+			filePath = fmt.Sprintf("../bfs/result_multi_BFS/%s_multi_bfs_level.json", req.Target)
+		}
+	case "DFS":
+		if req.Mode == "single" {
+			filePath = fmt.Sprintf("../dfs/result_BFS/%s_bfs.json", req.Target)
+		} else {
+			filePath = fmt.Sprintf("../dfs/result_multi_BFS/%s_multi_bfs_level.json", req.Target)
+		}
+	default:
+		http.Error(w, "Algoritma tidak ada", http.StatusBadRequest)
+		return
+	}
+
+	jsonFile, err := os.Open(filePath)
 	if err != nil {
 		fmt.Println("Error opening JSON file:", err)
 		http.Error(w, "Error reading file", http.StatusInternalServerError)
 		return
 	}
-
 	defer jsonFile.Close()
 
-	var trees Node
-	err = json.NewDecoder(jsonFile).Decode(&trees)
+	var response SearchResponse
+	err = json.NewDecoder(jsonFile).Decode(&response)
 	if err != nil {
 		fmt.Println("Error decoding JSON file:", err)
 		http.Error(w, "Error parsing tree data", http.StatusInternalServerError)
 		return
-	}
-
-	response := SearchResponse{
-		Trees:         trees,
-		NumberOfPaths: 1,
-		NodesVisited:  11,
-		ElapsedTime:   0.23,
-	}
-
-	treeJSON, err := json.MarshalIndent(response, "", "  ")
-	if err != nil {
-		fmt.Println("Error marshalling response:", err)
-	} else {
-		fmt.Println(string(treeJSON))
 	}
 
 	w.Header().Set("Content-Type", "application/json")
